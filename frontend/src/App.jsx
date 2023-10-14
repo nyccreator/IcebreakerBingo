644daad5e5d5de17959fac6d76ea899257e82b60
Amy Truong <avytrv@gmail.com>
2023-10-14 11:10:55 -0400

    Add inputs to update a bingo entries array

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // Test call to api/hello endpoint
+  // TODO: Remove this code in final product
   useEffect(() => {
     async function fetchData() {
       const response = await fetch('api/hello')
@@ -15,28 +14,41 @@ function App() {
     fetchData()
   }, [])
 
-  
+  const [entries, setEntries] = useState([
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', 'free', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+  ])
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      {entries.map((rowData, row) => (
+        <div
+          key={`row-${row}`}
+          className="form-row"
+        >
+          {rowData.map((entryData, col) => (
+              <input
+                key={`row-${row}-col-${col}`}
+                type="text"
+                value={entries[row][col]}
+                onInput={(e) => {
+                  setEntries((prev) => {
+                    const result = [...prev]
+                    result[row][col] = e.target.value
+                    return result
+                  })
+                  console.log(entries)
+                }}
+              />
+            ))
+          }
+        </div>
+      ))}
+      <p>
+        {'Entries array: ' + entries.toString()}
       </p>
     </>
   )
