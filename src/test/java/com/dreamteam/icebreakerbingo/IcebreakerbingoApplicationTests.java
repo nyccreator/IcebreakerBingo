03fce68964cc578c1592295af085ef480a080c67
Amy Truong <avytrv@gmail.com>
2023-10-13 23:30:34 -0400

    Initialize React/Vite/Spring project

---
+++ src/test/java/com/dreamteam/icebreakerbingo/IcebreakerbingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamteam.icebreakerbingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcebreakerbingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
