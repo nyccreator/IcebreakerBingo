03a95961dafd02c89874503d25b6f3b5121b94ea
Amy Truong <avytrv@gmail.com>
2023-10-15 01:42:09 -0400

    Edit backend to connect to MongoDB Atlas

--- src/main/java/com/dreamteam/icebreakerbingo/BingoCardApplication.java
+++ src/main/java/com/dreamteam/icebreakerbingo/BingoCardApplication.java
@@ -4,15 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class BingoCardApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
